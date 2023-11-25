0fc98a8e04a1d7ff79a9841a6cdb07a63ff63bfd
Adam "AdamIx" Burdík <110720675+AdamBurdik@users.noreply.github.com>
2023-11-25 13:21:43 +0100

    Update main.py
    added api

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def root():
     return render_template('index.html')
+
+@app.route('/api/player', methods=["POST"])
+def player():
+    return jsonify({"rest": "hi"})
 
 if __name__ == '__main__':
     app.run()
