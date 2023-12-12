63d1bac8e2d3b008da69d744b610956f1f8ab138
AdamIx <adamburdik9@gmail.com>
2023-12-12 16:10:25 +0100

    update

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import shutil
 
 app = Flask(__name__, static_url_path='/static')
 
-@app.route('/api/resourcepack', methods=["GET"])
+@app.route('/api/resourcepack')
 def api_resourcepack():
-    return send_file("strangcraft_pack.zip", download_name="strangecraft_pack.zip", as_attachment=True)
+    return send_file("strangecraft_pack.zip", as_attachment=True, download_name='strangecraft_pack.zip', mimetype='application/zip', cache_timeout=0)
 
 if __name__ == '__main__':
     #shutil.make_archive("pack", 'zip', "zipped_pack")
