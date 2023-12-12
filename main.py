b3d0d6705b69981dfdd24260b1248dd1e8ea6b02
AdamIx <adamburdik9@gmail.com>
2023-12-12 16:25:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask, send_file, jsonify, send_from_directory
-import shutil
-import os
+from flask import Flask
 # from dotenv import load_dotenv
-# import hashlib
 
 # load_dotenv()
 
@@ -12,25 +9,5 @@ app = Flask(__name__, static_url_path='/static')
 def root():
     return "Root!"
 
-@app.route('/api/resourcepack/', methods=["GET"])
-def api_resourcepack():
-    try:
-        # Use the current working directory as the resource pack directory
-        resource_pack_directory = os.getcwd()
-        
-        # Specify the filename of your Minecraft resource pack file
-        resource_pack_filename = 'strangecraft_pack.zip'
-
-        # Serve the file with Flask-Static-Compress
-        return send_from_directory(resource_pack_directory, resource_pack_filename, as_attachment=True, download_name='strangecraft_pack.zip')
-
-    except Exception as e:
-        # Log the exception for debugging purposes
-        print(f"Error serving resource pack: {e}")
-
-        # Alternatively, you can return a custom error response
-        return "Internal Server Error", 500
-
 if __name__ == '__main__':
-    #shutil.make_archive("pack", 'zip', "zipped_pack")
     app.run()
