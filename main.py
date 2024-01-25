96c5ef49903c16480a9aa8bd27f69877d5d63ad1
Adam "AdamIx" Burdík <110720675+AdamBurdik@users.noreply.github.com>
2024-01-25 18:24:50 +0100

    Update main.py
    Added route /nabor/

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 # from dotenv import load_dotenv
 
 # load_dotenv()
@@ -7,7 +7,15 @@ app = Flask(__name__, static_url_path='/static')
 
 @app.route("/")
 def root():
-    return "Root!"
+    return render_template('index.html')
+
+@app.route("/nabor/builder")
+def root():
+    return redirect("https://forms.gle/16Z2TFpDVY2fbk2G9")
+
+@app.route("/nabor/helper")
+def root():
+    return redirect("https://forms.gle/xqiLifQ7MFaSoMTh7")
 
 if __name__ == '__main__':
     app.run()
